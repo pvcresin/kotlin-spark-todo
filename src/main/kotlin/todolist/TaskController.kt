721965bd1474304516d429ad62dbddb1e5b64627
pvcresin <pvcresin0730@gmail.com>
2020-02-04 13:03:03 +0900

    Init

---
+++ src/main/kotlin/todolist/TaskController.kt
@@ -0,0 +1,52 @@
+package todolist
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import spark.Request
+import spark.Route
+import spark.Spark.halt
+import java.lang.Exception
+
+class TaskController(
+    private val objectMapper: ObjectMapper,
+    private val taskRepository: TaskRepository
+) {
+    private val Request.task: Task?
+        get() {
+            val id = params("id").toLongOrNull()
+            return id?.let(taskRepository::findById)
+        }
+
+    fun index(): Route = Route { _, _ ->
+        taskRepository.findAll()
+    }
+
+    fun create(): Route = Route { req, res ->
+        val request: TaskCreateRequest =
+            objectMapper.readValue(req.bodyAsBytes()) ?: throw halt(400)
+        val task = taskRepository.create(request.content)
+        res.status(201)
+        task
+    }
+
+    fun show(): Route = Route { req, _ ->
+        req.task ?: throw halt(404)
+    }
+
+    fun update(): Route = Route { req, res ->
+        val request: TaskUpdateRequest =
+            objectMapper.readValue(req.bodyAsBytes()) ?: throw halt(400)
+        val task = req.task ?: throw halt(404)
+        val newTask = task.copy(
+            content = request.content ?: task.content,
+            done = request.done ?: task.done
+        )
+        taskRepository.update(newTask)
+        res.status(204)
+    }
+
+    fun destroy() = Route { req, res ->
+        val task = req.task ?: throw halt(404)
+        taskRepository.delete(task)
+        res.status(204)
+    }
+}
---
+++ src/main/kotlin/todolist/TaskRepository.kt
@@ -0,0 +1,30 @@
+package todolist
+
+class TaskRepository {
+    private val tasks: MutableList<Task> = mutableListOf();
+
+    private val maxId: Long
+        get() = tasks.map(Task::id).max() ?: 0
+
+    fun findAll(): List<Task> = tasks.toList()
+
+    fun create(content: String): Task {
+        val id = maxId + 1
+        val task = Task(id, content, false)
+        tasks += task
+        return task
+    }
+
+    fun findById(id: Long): Task? = tasks.find { it.id == id }
+
+    fun update(task: Task) {
+        tasks.replaceAll{ t ->
+            if (t.id == task.id) task
+            else t
+        }
+    }
+
+    fun delete(task: Task) {
+        tasks.removeIf { it.id == task.id }
+    }
+}
